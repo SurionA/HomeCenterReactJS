41a9639989b31504f8787f9b78355ae7c53e30e0
SurionA <frere.maxime@gmail.com>
2018-04-09 13:17:17 +0200

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import dotenv from 'dotenv';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
