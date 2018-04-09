71200569cf53765ce680f09e7e4b4008e4efec38
SurionA <frere.maxime@gmail.com>
2018-04-09 13:21:01 +0200

    Create CitySelect component

---
+++ src/CitySelect/index.js
@@ -0,0 +1,57 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import VirtualizedSelect from 'react-virtualized-select';
+import 'react-select/dist/react-select.css';
+import 'react-virtualized-select/styles.css';
+
+import CITIES from '../Fixtures/cities-fr.json';
+import { selectStyle, titleStyle } from './styles';
+
+export default class CitySelect extends Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.state = {
+      selectedCity: {
+        id: CITIES[0].id,
+        nm: CITIES[0].nm,
+      },
+    };
+  }
+
+  onChange(city) {
+    if (city) {
+      this.setState({
+        selectedCity: {
+          id: city.id,
+          nm: city.nm,
+        },
+      });
+
+      this.props.onChangeCity(city.id);
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h3 style={titleStyle}>Sélectionner une ville</h3>
+        <VirtualizedSelect
+          style={selectStyle}
+          options={CITIES}
+          value={this.state.selectedCity}
+          clearable={false}
+          name="select-city"
+          onChange={this.onChange}
+          searchable
+          labelKey="nm"
+          valueKey="id"
+        />
+      </Fragment>
+    );
+  }
+}
+
+CitySelect.propTypes = {
+  onChangeCity: PropTypes.func.isRequired,
+};
---
+++ src/CitySelect/styles.js
@@ -0,0 +1,13 @@
+export const selectStyle = {
+  margin: 'auto',
+  minWidth: '350px',
+  color: 'var(--secondary-font-color)',
+};
+
+export const titleStyle = {
+    textAlign: 'left',
+}
+
+export default {
+  selectStyle,
+};
